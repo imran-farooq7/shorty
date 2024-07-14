ff67080f27eadb4516bb74874020de439ee7d065
Imran Farooq <imran_farooq7@hotmail.com>
2024-07-14 05:01:58 +0500

    Mobile menu completed

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,10 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
 	return (
 		<nav className="relative container mx-auto py-6">
 			<div className="flex items-center justify-between">
@@ -37,6 +40,48 @@ const Navbar = () => {
 						className="px-8 py-3 bg-cyan text-white rounded-full hover:opacity-70"
 					>
 						Sign up
+					</Link>
+				</div>
+				<button
+					id="menu-btn"
+					className={`block hamburger lg:hidden focus:outline-none ${
+						isOpen && "open"
+					}`}
+					type="button"
+					onClick={() => setIsOpen((isOpen) => !isOpen)}
+				>
+					<span className={`hamburger-top ${isOpen && "open"}`}></span>
+					<span className={`hamburger-middle ${isOpen && "open"}`}></span>
+					<span className={`hamburger-bottom ${isOpen && "open"}`}></span>
+				</button>
+			</div>
+			<div
+				className={`absolute ${
+					isOpen ? "block" : "hidden"
+				} p-6 rounded-lg bg-darkViolet left-6 top-20 right-6 z-[100]`}
+			>
+				<div className="flex flex-col items-center justify-center w-full space-y-6 font-bold text-white rounded-sm">
+					<Link href="#" className="w-full text-center">
+						Features
+					</Link>
+					<Link href="#" className="w-full text-center">
+						Pricing
+					</Link>
+					<Link href="#" className="w-full text-center">
+						Resources
+					</Link>
+					<Link
+						href="#"
+						className="w-full pt-6 border-t border-gray-400 text-center"
+					>
+						Login
+					</Link>
+
+					<Link
+						href="#"
+						className="w-full py-3 text-center rounded-full bg-cyan"
+					>
+						Sign Up
 					</Link>
 				</div>
 			</div>
